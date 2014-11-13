9ea21893723b3e0a58bd59254c34724ccefe9006
whitecollar <white.collar@mail.ru>
2014-11-13 03:06:16 +0000

    утро хуютро УБРАННЫ КРОШКИ

--- qiwi/themes/bootstrap/views/layouts/main.php
+++ qiwi/themes/bootstrap/views/layouts/main.php
@@ -43,11 +43,7 @@
 
 <div class="container" id="page">
 
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+
 
 	<?php echo $content; ?>
 
