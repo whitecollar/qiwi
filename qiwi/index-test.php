968a5224cfd133888e4c8ab8e7eb15cea342fd74
whitecollar <white.collar@mail.ru>
2014-11-13 04:06:29 +0000

    hh

--- qiwi/index-test.php
+++ qiwi/index-test.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * This is the bootstrap file for test application.
- * This file should be removed when the application is deployed for production.
- */
 
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../framework/yii.php';
