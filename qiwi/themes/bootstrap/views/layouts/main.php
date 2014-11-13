a82078b9168979d5d5180392835ee578fe17f7cc
whitecollar <white.collar@mail.ru>
2014-11-13 00:57:08 +0000

    утро хуютро поправлена верстка на таблицах

--- qiwi/themes/bootstrap/views/layouts/main.php
+++ qiwi/themes/bootstrap/views/layouts/main.php
@@ -42,7 +42,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by My Companrrrry.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
