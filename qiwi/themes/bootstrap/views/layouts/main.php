d573b62a6db52aaf2b23ef6745b1d389de074457
whitecollar <white.collar@mail.ru>
2014-11-13 02:26:09 +0000

    утро хуютро htcgjycbd if,kjy r ,encnhfge

--- qiwi/themes/bootstrap/views/layouts/main.php
+++ qiwi/themes/bootstrap/views/layouts/main.php
@@ -3,9 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
-
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
+	<meta name="language" content="ru" />
+       
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
