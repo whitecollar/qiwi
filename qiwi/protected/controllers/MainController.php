a3253ecdcaa1b552eb29382e24af09a0fd6c030f
whitecollar <white.collar@mail.ru>
2014-11-14 02:48:05 +0000

    main menu

--- qiwi/protected/controllers/MainController.php
+++ qiwi/protected/controllers/MainController.php
@@ -23,26 +23,22 @@ class MainController extends Controller
 	 * This method is used by the 'accessControl' filter.
 	 * @return array access control rules
 	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+  public function accessRules()
+    {
+      return array(
+        array('allow',
+            'actions'=>array('index','view','group','create','update'),
+            'roles'=>array('user'),
+        ),
+        array('allow',
+            'actions'=>array('delete','admin'),
+            'roles'=>array('root'),
+        ),
+        array('deny',  // deny all users
+            'users'=>array('*'),
+        ),
+        );
+    }
 
 	/**
 	 * Displays a particular model.
