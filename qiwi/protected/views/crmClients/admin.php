1af119f0ef8f363b9851530be65366215a2f43d6
whitecollar <white.collar@mail.ru>
2014-11-18 04:46:26 +0000

    Клиенты добавленно поле дата заведения и клиенты в общем

--- qiwi/protected/views/crmClients/admin.php
+++ qiwi/protected/views/crmClients/admin.php
@@ -5,7 +5,7 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List CrmClients','url'=>array('index')),
+	
 	array('label'=>'Create CrmClients', 'url'=>array('create'), 'linkOptions'=>array(
 		'ajax' => array(
 			'url'=>$this->createUrl('create'),
@@ -31,25 +31,25 @@ $('.search-form form').submit(function(){
 <?php $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'TBDialogCrud')); ?>
 <?php $this->endWidget(); ?>
 
-<h1>Manage Crm Clients</h1>
+<h1>Клиенты</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button btn')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+  <?php
+            $this->beginWidget('zii.widgets.CPortlet', array(
+                'title'=>' ',
+            ));
+            $this->widget('bootstrap.widgets.TbMenu', array(
+                'items'=>$this->menu,
+                'htmlOptions'=>array('class'=>'operations'),
+            ));
+            $this->endWidget();
+        ?>
 
 <?php $this->widget('bootstrap.widgets.TbGridView',array(
 	'id'=>'crm-clients-grid',
 	'ajaxUpdate'=>false,
+        'type'          => 'striped bordered condensed',  
 	'dataProvider'=>$model->search(),
-	'filter'=>$model,
+	//'filter'=>$model,
 	'columns'=>array(
 		'id',
 		'type',
